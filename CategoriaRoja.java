fab738bd7d90a0f3f0f4055992978ff44f073ae5
Carlos Alberto Henández Arrieta <T43563@banxico.org.mx>
2020-02-07 14:07:49 -0600

    Java File

---
+++ CategoriaRoja.java
@@ -0,0 +1,8 @@
+public class MyClass {
+    public static void main(String[] args) {
+        /*
+        - Categoría Interna Uso Limitado
+        */
+        System.out.println("Hola Mundo");
+    }
+}
